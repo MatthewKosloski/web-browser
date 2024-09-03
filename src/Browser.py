140da53f7520f8d3c9ca498703a4442862aa0bfc
Matthew Kosloski <matthewkosloski@me.com>
2024-09-02 22:57:15 -0500

    lay out text word by word

--- src/Browser.py
+++ src/Browser.py
@@ -1,7 +1,11 @@
 import tkinter
+import tkinter.font
 
-from Url import Url
+from Layout import Layout
 from Scrollbar import Scrollbar
+from Tag import Tag
+from Text import Text
+from Url import Url
 
 class Browser:
 
@@ -41,8 +45,8 @@ class Browser:
 
     def load(self, url):
         body = url.request()
-        text = lex(body)
-        self.display_list = layout(text, self.width, self.hstep, self.vstep)
+        tokens = lex(body)
+        self.display_list = Layout(tokens, self.hstep, self.vstep, self.width).display_list
         self.scrollbar = Scrollbar(
             canvas=self.canvas,
             window_width=self.width,
@@ -60,49 +64,33 @@ class Browser:
     def draw(self, e = None):
         self.scrollbar.draw_scrollbar(e)
 
-        for page_x, page_y, c in self.display_list:
+        for page_x, page_y, word, font in self.display_list:
 
             # Skip drawing characters that are offscreen.
             if page_y > self.scrollbar.scroll + self.height: continue
             if page_y + self.vstep < self.scrollbar.scroll: continue
 
             screen_x, screen_y = self.to_screen_coordinate((page_x, page_y))
-            self.canvas.create_text(screen_x, screen_y, text=c)
-
-'''
-  Computes and stores the position of each character, storing
-  it in a display list. This function operates using page coordinates.
-'''
-def layout(text, window_width, hstep = 13, vstep = 18):
-    display_list = []
-    cursor_x, cursor_y = hstep, vstep
-
-    for c in text:
-        display_list.append((cursor_x, cursor_y, c))
-        cursor_x += hstep
-        # Wrap the text once we reach the edge of the screen.
-        # The ICU Library, used by both Firefox and Chrome, uses
-        # dynamic programming to guess phase boundaries based on
-        # a word frequency table.
-        # https://site.icu-project.org/
-        # https://github.com/unicode-org/icu/blob/master/icu4c/source/data/brkitr/dictionaries/cjdict.txt
-        if cursor_x >= window_width - hstep:
-            cursor_y += vstep
-            cursor_x = hstep
-
-    return display_list
+            self.canvas.create_text(screen_x, screen_y, text=word, font=font, anchor='nw')
 
 def lex(body):
-    text = ""
+    out = []
+    buffer = ""
     in_tag = False
     for c in body:
         if c == "<":
             in_tag = True
+            if buffer: out.append(Text(buffer))
+            buffer = ""
         elif c == ">":
             in_tag = False
-        elif not in_tag:
-            text += c
-    return text
+            out.append(Tag(buffer))
+            buffer = ""
+        else:
+            buffer += c
+    if not in_tag and buffer:
+        out.append(Text(buffer))
+    return out
 
 if __name__ == "__main__":
     import sys
---
+++ src/Layout.py
@@ -0,0 +1,106 @@
+import tkinter
+import tkinter.font
+
+from Tag import Tag
+from Text import Text
+
+class Layout:
+
+    LEADING = 1.25
+    FONTS = {}
+
+    def __init__(self, tokens, hstep, vstep, window_width):
+        self.display_list = []
+        self.line = []
+        self.hstep = hstep
+        self.vstep = vstep
+        self.window_width = window_width
+        self.cursor_x = hstep
+        self.cursor_y = vstep
+        self.weight = "normal"
+        self.style = "roman"
+        self.size = 12
+
+        for tok in tokens:
+            self.token(tok)
+
+        self.flush()
+    
+    def token(self, tok):
+        if isinstance(tok, Text):
+            for word in tok.text.split():
+                self.word(word)
+        elif tok.tag == "i":
+            self.style = "italic"
+        elif tok.tag == "/i":
+            self.style = "roman"
+        elif tok.tag == "b":
+            self.weight = "bold"
+        elif tok.tag == "/b":
+            self.weight = "normal"
+        elif tok.tag == "small":
+            self.size -= 2
+        elif tok.tag == "/small":
+            self.size += 2
+        elif tok.tag == "big":
+            self.size += 4
+        elif tok.tag == "/big":
+            self.size -= 4
+        elif tok.tag == "br":
+            # End the current line and start a new one.
+            self.flush()
+        elif tok.tag == "/p":
+            # End the current line and start a new one.
+            self.flush()
+            # Add a gap between paragraphs.
+            self.cursor_y += self.vstep
+
+    def word(self, word):
+        font = self.get_font(self.size, self.weight, self.style)
+        w = font.measure(word)
+
+        if self.cursor_x + w > self.window_width - self.hstep:
+            # Wrap text to next line.
+            self.cursor_y += font.metrics("linespace") * self.LEADING
+            self.cursor_x = self.hstep
+            
+            self.flush()
+
+        # self.display_list.append((self.cursor_x, self.cursor_y, word, font))
+        self.line.append((self.cursor_x, word, font))
+
+        # " " adds back the whitespace that was removed when splitting the text.
+        self.cursor_x += w + font.measure(" ")
+
+    def flush(self):
+        if not self.line: return
+        metrics = [font.metrics() for x, word, font in self.line]
+
+        # The top of the tallest glyph.
+        max_ascent = max(metric["ascent"] for metric in metrics)
+
+        baseline = self.cursor_y + self.LEADING * max_ascent
+
+        # Align each word vertically relative to the baseline.
+        for x, word, font in self.line:
+            y = baseline - font.metrics("ascent")
+            self.display_list.append((x, y, word, font))
+
+        # The bottom of the deepest glyph.
+        max_descent = max([metric["descent"] for metric in metrics])
+
+        self.cursor_y = baseline + self.LEADING * max_descent
+
+        self.cursor_x = self.hstep
+        self.line = []
+
+    def get_font(self, size, weight, style):
+        key = (size, weight, style)
+
+        if key not in self.FONTS:
+            font = tkinter.font.Font(size=size, weight=weight, slant=style)
+            label = tkinter.Label(font=font)
+            self.FONTS[key] = (font, label)
+
+        return self.FONTS[key][0]
+    
