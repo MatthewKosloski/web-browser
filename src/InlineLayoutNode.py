ea41eb82042a8422a5f13732a811b7f6a031bdcf
Matthew Kosloski <matthewkosloski@me.com>
2024-09-19 01:35:26 -0500

    feat: created InlineLayoutNode

--- src/InlineLayoutNode.py
+++ src/InlineLayoutNode.py
@@ -1,24 +1,14 @@
 import tkinter
 import tkinter.font
 
-from DrawRect import DrawRect
 from DrawText import DrawText
 from Element import Element
 from Text import Text
-from InlineLayoutNode import InlineLayoutNode
 
-class BlockLayoutNode:
+class InlineLayoutNode:
 
     LEADING = 1.25
     FONTS = {}
-    BLOCK_ELEMENTS = [
-        "html", "body", "article", "section", "nav", "aside",
-        "h1", "h2", "h3", "h4", "h5", "h6", "hgroup", "header",
-        "footer", "address", "p", "hr", "pre", "blockquote",
-        "ol", "ul", "menu", "li", "dl", "dt", "dd", "figure",
-        "figcaption", "main", "div", "table", "form", "fieldset",
-        "legend", "details", "summary"
-    ]
 
     def __init__(self, config, node, parent, previous):
         self.config = config
@@ -34,15 +24,8 @@ class BlockLayoutNode:
 
     def paint(self):
         commands = []
-
-        if isinstance(self.node, Element) and self.node.tag == "pre":
-            x2, y2 = self.x + self.width, self.y + self.height
-            rect = DrawRect(self.x, self.y, x2, y2, "gray")
-            commands.append(rect)
-
-        if self.layout_mode() == "inline":
-            for x, y, word, font in self.display_list:
-                commands.append(DrawText(x, y, word, font))
+        for x, y, word, font in self.display_list:
+            commands.append(DrawText(x, y, word, font))
         return commands
 
     def layout(self):
@@ -58,46 +41,24 @@ class BlockLayoutNode:
         else:
             self.y = self.parent.y
 
-
-        mode = self.layout_mode()
         self.width = self.parent.width
 
-        if mode == "block":
-            previous = None
-            for child in self.node.children:
-                if child.tag in self.BLOCK_ELEMENTS:
-                    next = BlockLayoutNode(self.config, child, self, previous)
-                else:
-                    next = InlineLayoutNode(self.config, child, self, previous)
-                self.children.append(next)
-                previous = next
-        else:
-            previous = None
-            next = InlineLayoutNode(self.config, self.node, self, previous)
-            self.children.append(next)
-            previous = next
+        self.cursor_x = 0
+        self.cursor_y = 0
+        self.weight = "normal"
+        self.style = "roman"
+        self.size = 12
+
+        self.line = []
+        self.recurse(self.node)
+
+        # We need to be tall enough to contain the text.
+        self.height = self.cursor_y
+
+        self.flush()
 
         for child in self.children:
             child.layout()
-
-        if mode == "block":
-            # Height is the sum of all the children's heights. Therefore,
-            # we need to compute the height after laying out the children.
-            self.height = sum([child.height for child in self.children])
-
-    def layout_mode(self):
-        if isinstance(self.node, Text):
-            return "inline"
-        elif self.node.tag in self.BLOCK_ELEMENTS:
-            return "block"
-        elif any([isinstance(child, Element) and \
-                child.tag in self.BLOCK_ELEMENTS
-                for child in self.node.children]):
-            return "block"
-        elif self.node.children:
-            return "inline"
-        else:
-            return "block"
 
     def recurse(self, tree):
         if isinstance(tree, Text):
@@ -188,7 +149,7 @@ class BlockLayoutNode:
     
     def __repr__(self):
         if isinstance(self.node, Text):
-            return "BlockLayoutNode('" + self.node.text + "')"
+            return "InlineLayoutNode('" + self.node.text + "')"
         else:
-            return "BlockLayoutNode(" + self.node.tag + ")"
+            return "InlineLayoutNode(" + self.node.tag + ")"
     
