ac311ca2303ecf7f95ecc7d008f5255e8f2dc511
Matthew Kosloski <matthewkosloski@me.com>
2024-11-01 01:45:04 -0500

    refactor(browser): cleanup

--- src/browser.py
+++ src/browser.py
@@ -23,67 +23,38 @@ class Browser:
         self.window.bind("<Down>", self.handle_event_down)
         self.window.bind("<Up>", self.handle_event_up)
 
-    def handle_event_down(self, e):
-        if self.scrollbar is not None:
-            self.scrollbar.scroll_down(e)
-        self.draw(e)
-
-    def handle_event_up(self, e):
-        if self.scrollbar is not None:
-            self.scrollbar.scroll_up(e)
-        self.draw(e)
-
-    def print_tree(self, node, indent=0):
-        print(" " * indent, node)
-        for child in node.children:
-            self.print_tree(child, indent + 4)
-
-
     def load(self, url):
+        # Get HTML document, either from disk or the internet.
         body = url.request()
-        nodes = HTMLParser(body).parse()
 
-        self.style(nodes)
+        # Parse the HTML document, returning a tree of nodes.
+        nodes = HTMLParser(body).parse()
         
         print("HTML tree:")
-        self.print_tree(nodes)
+        self.log_tree(nodes)
 
+        # Apply a CSS parser to each node's style attribute.
+        self.style(nodes)
+
+        # From the HTML tree, produce a layout tree with a root DocumentLayoutNode.
         self.document = DocumentLayoutNode(nodes)
         self.document.layout()
 
-        self.scrollbar = Scrollbar(self.canvas, self.document.height)
-                
         print("Layout tree:")
-        self.print_tree(self.document)
+        self.log_tree(self.document)
+
+        # Create a scrollbar using the height of the root node of the layout tree.
+        self.scrollbar = Scrollbar(self.canvas, self.document.height)
         
+        # From the layout tree, produce a linear list of draw commands.
         self.display_list = []
-        self.paint_tree(self.document, self.display_list)
+        self.paint(self.document, self.display_list)
 
         print("Draw commands:")
         print(self.display_list)
 
+        # Execute each draw command.
         self.draw()
-
-    def to_screen_coordinate(self, page_coordinate):
-        page_x, page_y = page_coordinate
-        screen_x, screen_y = (page_x, page_y - self.scrollbar.scroll)
-
-        return (screen_x, screen_y)
-
-    def draw(self, e = None):
-        self.canvas.delete("all")
-        self.scrollbar.draw(e)
-
-        for command in self.display_list:
-            if command.top > self.scrollbar.scroll + WINDOW_HEIGHT: continue
-            if command.bottom < self.scrollbar.scroll: continue
-            command.execute(self.scrollbar.scroll, self.canvas)
-
-    def paint_tree(self, layout_object, display_list):
-        display_list.extend(layout_object.paint())
-
-        for child in layout_object.children:
-            self.paint_tree(child, display_list)
 
     def style(self, node):
         node.style = {}
@@ -96,6 +67,42 @@ class Browser:
 
         for child in node.children:
             self.style(child)
+
+    def paint(self, layout_object, display_list):
+        display_list.extend(layout_object.paint())
+
+        for child in layout_object.children:
+            self.paint(child, display_list)
+
+    def draw(self, e = None):
+        self.canvas.delete("all")
+        self.scrollbar.draw(e)
+
+        for command in self.display_list:
+            if command.top > self.scrollbar.scroll + WINDOW_HEIGHT: continue
+            if command.bottom < self.scrollbar.scroll: continue
+            command.execute(self.scrollbar.scroll, self.canvas)
+
+    def to_screen_coordinate(self, page_coordinate):
+        page_x, page_y = page_coordinate
+        screen_x, screen_y = (page_x, page_y - self.scrollbar.scroll)
+
+        return (screen_x, screen_y)
+
+    def handle_event_down(self, e):
+        if self.scrollbar is not None:
+            self.scrollbar.scroll_down(e)
+        self.draw(e)
+
+    def handle_event_up(self, e):
+        if self.scrollbar is not None:
+            self.scrollbar.scroll_up(e)
+        self.draw(e)
+
+    def log_tree(self, node, indent=0):
+        print(" " * indent, node)
+        for child in node.children:
+            self.log_tree(child, indent + 4)
 
 if __name__ == "__main__":
     import sys
