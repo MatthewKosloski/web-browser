9038bdcf3cadf2e5cb6e3e40cc518d4112c78181
Matthew Kosloski <matthewkosloski@me.com>
2024-11-20 22:43:20 -0600

    feat(browser): handle click event

--- src/browser.py
+++ src/browser.py
@@ -2,6 +2,7 @@ import tkinter
 
 from constants import WINDOW_WIDTH, WINDOW_HEIGHT
 from css.style_computer import StyleComputer
+from hypertext.nodes import Text
 from hypertext.parser import HTMLParser
 from layout.document_layout_node import DocumentLayoutNode
 from scrollbar import Scrollbar
@@ -10,6 +11,7 @@ from url.url import Url
 class Browser:
     def __init__(self):
         self.scrollbar = None
+        self.url = None
         self.window = tkinter.Tk()
         self.canvas = tkinter.Canvas(
             self.window,
@@ -21,8 +23,11 @@ class Browser:
         # Bind events to event handlers.
         self.window.bind("<Down>", self.handle_event_down)
         self.window.bind("<Up>", self.handle_event_up)
+        self.window.bind("<Button-1>", self.handle_event_click)
 
     def load(self, url):
+        self.url = url
+
         # Get HTML document, either from disk or the internet.
         body = url.request()
 
@@ -83,10 +88,44 @@ class Browser:
             self.scrollbar.scroll_up(e)
         self.draw(e)
 
+    def handle_event_click(self, e):
+
+        # Convert the screen coordinates to page coordinates.
+        x, y = e.x, e.y
+        y += self.scrollbar.scroll
+
+        # Perform a hit test. Get the list of layout objects that have been clicked.
+        objs = [obj for obj in self.tree_to_list(self.document, [])
+            if obj.x <= x < obj.x + obj.width
+            and obj.y <= y < obj.y + obj.height]
+        
+        # Early return if no layout object was clicked on.
+        if not objs: return
+
+        # The most specific node that was clicked is the last element in the hit test.
+        elt = objs[-1].node
+
+        while elt:
+            if isinstance(elt, Text):
+                pass
+            elif elt.tag == "a" and "href" in elt.attributes:
+                # A link element was clicked, so extract the URL and load it.
+                url = self.url.resolve(elt.attributes["href"])
+                return self.load(url)
+            elt = elt.parent
+        
+
+
     def log_tree(self, node, indent=0):
         print(" " * indent, node)
         for child in node.children:
             self.log_tree(child, indent + 4)
+
+    def tree_to_list(self, tree, list):
+        list.append(tree)
+        for child in tree.children:
+            self.tree_to_list(child, list)
+        return list
 
 if __name__ == "__main__":
     import sys
