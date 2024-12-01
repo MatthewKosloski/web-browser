eb8364a7d0fc66844a5cdad3a76c51d66c775245
Matthew Kosloski <matthewkosloski@me.com>
2024-11-30 18:16:12 -0600

    chore: add type hints to src/painting

--- src/painting/commands.py
+++ src/painting/commands.py
@@ -1,9 +1,14 @@
+from tkinter import Canvas
+from tkinter.font import Font
+
+from painting.shapes import Rect
+
 class DrawRect:
-    def __init__(self, rect, color):
+    def __init__(self, rect: Rect, color: str) -> None:
         self.rect = rect
         self.color = color
 
-    def execute(self, canvas, scroll = 0):
+    def execute(self, canvas: Canvas, scroll = 0) -> None:
         canvas.create_rectangle(
             self.rect.left,
             self.rect.top - scroll,
@@ -13,18 +18,18 @@ class DrawRect:
             width=0,
             fill=self.color)
         
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"DrawRect(({self.rect.left}, {self.rect.top}), ({self.rect.right}, {self.rect.bottom}), {self.color})"
         
 class DrawText:
-    def __init__(self, rect, text, font, color):
+    def __init__(self, rect: Rect, text: str, font: Font, color: str) -> None:
         rect.bottom = rect.top + font.metrics("linespace")
         self.rect = rect
         self.text = text
         self.font = font
         self.color = color
 
-    def execute(self, canvas, scroll = 0):
+    def execute(self, canvas: Canvas, scroll = 0) -> None:
         canvas.create_text(
             self.rect.left,
             self.rect.top - scroll,
@@ -33,36 +38,36 @@ class DrawText:
             fill=self.color,
             anchor='nw')
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"DrawText(({self.rect.left}, {self.rect.top}), \"{self.text}\", {self.color})"
     
 class DrawOutline:
-    def __init__(self, rect, color, thickness):
+    def __init__(self, rect: Rect, color: str, thickness: float) -> None:
         self.rect = rect
         self.color = color
         self.thickness = thickness
 
-    def execute(self, canvas, scroll = 0):
+    def execute(self, canvas: Canvas, scroll = 0) -> None:
         canvas.create_rectangle(
             self.rect.left, self.rect.top - scroll,
             self.rect.right, self.rect.bottom - scroll,
             width = self.thickness,
             outline = self.color)
     
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"DrawOutline({self.rect}, {self.color}, {self.thickness})"
     
 class DrawLine:
-    def __init__(self, rect, color, thickness):
+    def __init__(self, rect: Rect, color: str, thickness: float) -> None:
         self.rect = rect
         self.color = color
         self.thickness = thickness
 
-    def execute(self, canvas, scroll = 0):
+    def execute(self, canvas: Canvas, scroll = 0) -> None:
         canvas.create_line(
             self.rect.left, self.rect.top - scroll,
             self.rect.right, self.rect.bottom - scroll,
             fill=self.color, width=self.thickness)
     
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"DrawLine(({self.x1}, {self.y1}), ({self.x2}, {self.y2}), {self.color}, {self.thickness})"
--- src/painting/shapes.py
+++ src/painting/shapes.py
@@ -1,10 +1,10 @@
 class Rect:
-    def __init__(self, left = 0, top = 0, right = 0, bottom = 0):
+    def __init__(self, left = 0, top = 0, right = 0, bottom = 0) -> None:
         self.left = left
         self.top = top
         self.right = right
         self.bottom = bottom
 
-    def containsPoint(self, x, y):
+    def containsPoint(self, x: int, y: int) -> bool:
         return x >= self.left and x < self.right \
             and y >= self.top and y < self.bottom
