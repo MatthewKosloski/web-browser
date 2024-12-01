61298a559894a3863084041ac15053c91ed711e0
Matthew Kosloski <matthewkosloski@me.com>
2024-11-30 23:52:16 -0600

    chore(LayoutNode): make layout node an abstract base class

--- src/layout/layout_node.py
+++ src/layout/layout_node.py
@@ -1,15 +1,25 @@
 from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import List 
+from painting.commands import DrawCommand
 
 from hypertext.nodes import HTMLNode
 
-class LayoutNode:
+class LayoutNode(ABC):
     def __init__(self, node: HTMLNode, parent: LayoutNode = None, previous: LayoutNode = None) -> None:
         self.node = node
         self.parent = parent
         self.previous = previous
-        self.children = []
+        self.children: List[LayoutNode] = []
         self.x = None
         self.y = None
         self.width = None
         self.height = None
 
+    @abstractmethod
+    def layout() -> None:
+        pass
+
+    @abstractmethod
+    def paint() -> List[DrawCommand]:
+        pass
